ac28c7bf4b454262bfc94cd922031521da8093b5
shibkwa <64547019+shibkwa@users.noreply.github.com>
2021-08-16 23:56:18 +0900

    dsfds

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
-import ColorfulMessage from "./Components/ColorfulMessage";
+import { useEffect, useState } from "react";
+import { ColorfulMessage } from "./Components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwichShowFlag}>On/Off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ^ω^ )</p>}
     </>
   );
 };
--- src/Components/ColorfulMessage.jsx
+++ src/Components/ColorfulMessage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function ColorfulMessage(props) {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
     color: color,
     fontSize: "18px"
   };
   return <p style={contentStyle}>{children}</p>;
-}
+};
 
-export default ColorfulMessage;
+// export default ColorfulMessage;
